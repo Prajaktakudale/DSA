146b06e55413707b0c144189f8662714756c685d
prajakta <prajaktv@thoghtworks.com>
2013-12-10 16:38:34 +0530

    link list created

--- link_list/link_listTest.c
+++ link_list/link_listTest.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 void test_linked_list_is_created_initially_head_is_null(){
-        List* list = create_list();
+        DLL* list = create_list();
         ASSERT(NULL == list->start);
 };
 void test_linked_list_is_created_initially_size_is_0(){
-        List* list = create_list();
+        DLL* list = create_list();
         ASSERT(list->size == 0);
 };
