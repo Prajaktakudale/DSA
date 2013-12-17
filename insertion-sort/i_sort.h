4f3a48269ee4ff8964b92d9dd6ab3c809c4b1310
prajakta <prajaktv@thoghtworks.com>
2013-12-17 09:35:19 +0530

    insertion sort program deleted

--- insertion-sort/i_sort.h
+++
@@ -1,6 +0,0 @@
-#include <memory.h>
-typedef char String[256];
-typedef int (*compare)(void* first, void* second);
-
-void isort(void* base, size_t numberOfElements, size_t elementSize,
-			compare comp); 
