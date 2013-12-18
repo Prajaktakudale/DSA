78c5c21c0c78a7feb1d0ec39c8d6f36bb9e29d12
prajakta <prajaktv@thoghtworks.com>
2013-12-18 16:46:58 +0530

    testcase modified

--- os_schedular/os_schedularTest.c
+++ os_schedular/os_schedularTest.c
@@ -13,18 +13,18 @@ void test_for_create_new_process(){
 
 void test_for_add_process_in_schedular(){
         Queue expected = {NULL,0};
-        Status statusp1 = {0,1,0};
-        P p1 = {"pA",10,statusp1,3,NULL};
+        Status s = {0,1,0};
+        P p1 = {"pA",10,s,3,NULL};
         queue = createNewProcess();
         ASSERT(1 == add_process(queue, &p1));
         ASSERT(queue->head == &p1);
         ASSERT(queue->head->next == NULL);
 }
 void test_add_process_which_has_low_priority(){
-        Status statusp1 = {0,1,0};
+        Status s = {0,1,0};
         P *second,*third,*fourth,*fifth;
-        P p1 = {"p1",10,statusp1,5,NULL};
-        P p2 = {"p2",5,statusp1,1,NULL};
+        P p1 = {"p1",10,s,5,NULL};
+        P p2 = {"p2",5,s,1,NULL};
         queue = createNewProcess();
         ASSERT(1 == add_process(queue, &p1));
         ASSERT(2 == add_process(queue, &p2));
@@ -34,9 +34,9 @@ void test_add_process_which_has_low_priority(){
 }
 void test_for_add_new_process_which_has_higher_priority_than_exsisting_process(){
         Queue expected = {NULL,0};
-        Status statusp1 = {0,1,0};
-        P p1 = {"pA",10,statusp1,3,NULL};
-        P p2 = {"pB",5,statusp1,1,NULL};
+        Status s = {0,1,0};
+        P p1 = {"pA",10,s,3,NULL};
+        P p2 = {"pB",5,s,1,NULL};
         queue = createNewProcess();
         ASSERT(1 == add_process(queue, &p1));
         ASSERT(2 == add_process(queue, &p2));
@@ -44,21 +44,14 @@ void test_for_add_new_process_which_has_higher_priority_than_exsisting_process()
         ASSERT(queue->head->next == &p1);
 }
 void test_for_remove_process_between_two_processes(){
-        Status statusp1 = {0,1,0};
-        P pA = {"pA",10,statusp1,1,NULL};
-        P pB = {"pB",0,statusp1,5,NULL};
-        P pC = {"pC",10,statusp1,7,NULL};
+        Status s = {0,1,0};
+        P pA = {"pA",10,s,1,NULL};
+        P pB = {"pB",0,s,5,NULL};
+        P pC = {"pC",10,s,7,NULL};
         queue = createNewProcess();
         add_process(queue, &pA);
         add_process(queue, &pB);
         add_process(queue, &pC);
         ASSERT(2 == remove_process(queue));
         ASSERT(queue->head->next == &pC);
-}
-void test_for_delete_first_process_from_schedular(){
-        Status statusp1 = {0,1,0};
-        P pA = {"p1",0,statusp1,5,NULL};
-        queue = createNewProcess();
-        add_process(queue, &pA);
-        ASSERT(0 == remove_process(queue));
 }
