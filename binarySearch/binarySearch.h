d6b262438527f8adc01ca9561a2a15983a7fcb69
prajakta <prajaktv@thoghtworks.com>
2013-12-18 17:46:31 +0530

    duplicate folder deleted.

--- binarySearch/binarySearch.h
+++
@@ -1,2 +0,0 @@
-typedef char String[256];
-void* binary_search(void*key,void *base,int length, int size,int (*compare)(void*,void*));
--- stack_using_link_list/stack_using_link_list.h
+++
@@ -1,7 +0,0 @@
-#include "./need/link_list.h"
- 
-typedef DLL Stack;
-int push(Stack* start, void* data);
-void pop(Stack* start);
-int isEmpty(Stack* stack);
-void* top(Stack* stack);
