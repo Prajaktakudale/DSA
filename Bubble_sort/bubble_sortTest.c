a4e4a8b569059bc4dcef35f89fefe68f05027f24
prajakta <prajaktv@thoghtworks.com>
2013-12-28 09:32:19 +0530

    String testcase added, and modify other testcases.

--- Bubble_sort/bubble_sortTest.c
+++ Bubble_sort/bubble_sortTest.c
@@ -17,41 +17,53 @@ int compare_chars(void* element1, void* element2){
         return *(char*)element1 - *(char*)element2;
 };
 int compare_Strings(void* element1, void* element2){
-        return *(String*)element1 - *(String*)element2;
+        return strcmp(element1,element2);
 };
 void test_to_sort_the_elements_list_of_integers_datatype(){
-        int length = 4,i;
-        int before_sorting[4] = {8,7,1,4};
-        int after_sorting[4] = {1,4,7,8};
-        void* elements[4] = {&before_sorting[0],&before_sorting[1],&before_sorting[2],&before_sorting[3]};
-        bubble_sort(elements,4,compare_ints);
-        for(i=0;i<length;i++)
-                ASSERT(after_sorting[i] == *(int*)elements[i]);
+        int before_sorting[3] = {7,1,4};
+        int after_sorting[3] = {1,4,7};
+        void* elements[3] = {&before_sorting[0],&before_sorting[1],&before_sorting[2]};
+        bubble_sort(elements,3,compare_ints);
+        ASSERT(after_sorting[0] == *(int*)elements[0]);
+        ASSERT(after_sorting[1] == *(int*)elements[1]);
+        ASSERT(after_sorting[2] == *(int*)elements[2]);
 };
 void test_to_sort_the_elements_list_of_float_datatype(){
-        int length = 4,i;
-        float before_sorting[4] = {8.0f,10.0f,7.0f,4.0f};
-        float after_sorting[4] = {4.0f,7.0f,8.0f,10.0f};
-        void* elements[4] = {&before_sorting[0],&before_sorting[1],&before_sorting[2],&before_sorting[3]};
-        bubble_sort(elements,4,compare_floats);
-        for(i=0;i<length;i++)
-                ASSERT(after_sorting[i] == *(float*)elements[i]);
+        float before_sorting[3] = {8.0f,7.0f,4.0f};
+        float after_sorting[3] = {4.0f,7.0f,8.0f};
+        void* elements[3] = {&before_sorting[0],&before_sorting[1],&before_sorting[2]};
+        bubble_sort(elements,3,compare_floats);
+        ASSERT(after_sorting[0] == *(float*)elements[0]);
+        ASSERT(after_sorting[1] == *(float*)elements[1]);
+        ASSERT(after_sorting[2] == *(float*)elements[2]);
 };
 void test_to_sort_the_elements_list_of_double_datatype(){
-        int length = 4,i;
-        double before_sorting[4] = {8.0,10.0,7.0,4.0};
-        double after_sorting[4] = {4.0,7.0,8.0,10.0};
-        void* elements[4] = {&before_sorting[0],&before_sorting[1],&before_sorting[2],&before_sorting[3]};
-        bubble_sort(elements,4,compare_doubles);
-        for(i=0;i<length;i++)
-                ASSERT(after_sorting[i] == *(double*)elements[i]);
+        double before_sorting[3] = {8.0,7.0,4.0};
+        double after_sorting[3] = {4.0,7.0,8.0,};
+        void* elements[3] = {&before_sorting[0],&before_sorting[1],&before_sorting[2]};
+        bubble_sort(elements,3,compare_doubles);
+        ASSERT(after_sorting[0] == *(double*)elements[0]);
+        ASSERT(after_sorting[1] == *(double*)elements[1]);
+        ASSERT(after_sorting[2] == *(double*)elements[2]);
+
 };
 void test_to_sort_the_elements_list_of_character_datatype(){
-        int length = 4,i;
-        char before_sorting[4] = {'d','c','b','a'};
-        char after_sorting[4] = {'a','b','c','d'};
-        void* elements[4] = {&before_sorting[0],&before_sorting[1],&before_sorting[2],&before_sorting[3]};
-        bubble_sort(elements,4,compare_chars);
-        for(i=0;i<length;i++)
-                ASSERT(after_sorting[i] == *(char*)elements[i]);
+        char before_sorting[3] = {'d','c','b'};
+        char after_sorting[3] = {'b','c','d'};
+        void* elements[3] = {&before_sorting[0],&before_sorting[1],&before_sorting[2]};
+        bubble_sort(elements,3,compare_chars);
+        ASSERT(after_sorting[0] == *(char*)elements[0]);
+        ASSERT(after_sorting[1] == *(char*)elements[1]);
+        ASSERT(after_sorting[2] == *(char*)elements[2]);
+
+};
+void test_to_sort_the_elements_list_of_String_datatype(){
+        int length = 3,i;
+        String before_sorting[3] = {"dddd","bbcc","ccff"};
+        String after_sorting[3] = {"bbcc","ccff","dddd"};
+        void* elements[3] = {&before_sorting[0],&before_sorting[1],&before_sorting[2]};
+        bubble_sort(elements,3,compare_Strings);
+        ASSERT(0==strcmp(after_sorting[0],*(String*)elements[0]));
+        ASSERT(0==strcmp(after_sorting[1],*(String*)elements[1]));
+        ASSERT(0==strcmp(after_sorting[2],*(String*)elements[2]));
 };
